04a60c41dac2ddd238504cdbea87bb58f17eb2e2
sarit1983 <river.sarit@gmail.com>
2020-04-30 17:54:03 +0530

    Migration created for products and review

--- database/migrations/2020_04_30_112057_create_products_table.php
+++ database/migrations/2020_04_30_112057_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->text('Description');
+            $table->integer('stock');
+            $table->decimal('discount');
             $table->timestamps();
         });
     }
--- database/migrations/2020_04_30_112120_create_reviews_table.php
+++ database/migrations/2020_04_30_112120_create_reviews_table.php
@@ -15,7 +15,13 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->index();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('rating');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
